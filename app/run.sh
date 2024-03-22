81248d084956bb73b79eae2289c156a5711e0aaf
roberto-fernandino <romfernandino@gmail.com>
2024-03-22 17:34:02 -0300

    Initial Commit with project cloned

---
+++ app/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Function that stop second plans jobs this script started when pressing "ctrl+C"
+stop_jobs() {
+    jobs -p | xargs kill
+}
+
+cd backend
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+cd ..
+python backend/manage.py runserver & 
+cd frontend
+npm run dev -- --port 8080 &
+wait
