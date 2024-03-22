64ce5c085f704c6554b189d98c4faa2a5b65155b
roberto-fernandino <romfernandino@gmail.com>
2024-03-22 17:37:14 -0300

    Add new assets and TripText component

---
+++ app/frontend/src/components/TripText.vue
@@ -0,0 +1,13 @@
+<template>
+  <span id="text"
+    >Estas são as melhores alternativas de viagem para a data selecionada
+  </span>
+</template>
+
+<style scoped>
+#text {
+  font-weight: 500;
+  font-family: sans-serif;
+  margin-bottom: 2rem;
+}
+</style>
